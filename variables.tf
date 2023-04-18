f57ba888d2f0126211bc7d0eb17e5061f9692820
Terraform <romain.cambonie@gmail.com>
2023-04-18 09:14:22 +0200

    feat: adding terraform aws variables file

---
+++ variables.tf
@@ -0,0 +1,32 @@
+variable "project" {
+  type        = string
+  nullable    = false
+  description = "The name of the project that hosts the environment"
+}
+
+variable "service" {
+  type        = string
+  nullable    = false
+  description = "The name of the service that will be run on the environment"
+  default     = "client"
+}
+
+variable "task_definition_execution_role_arn" {
+  default = ""
+}
+
+variable "db_connexion_string" {
+  default = ""
+}
+
+variable "project_vpc_id" {
+  default = ""
+}
+
+variable "private_subnets_ids" {
+  default = ""
+}
+
+variable "public_subnets_id" {
+  default = ""
+}
