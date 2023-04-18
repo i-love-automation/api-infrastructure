89ef0d7e5896ed665669ac4222940a1c58e6928b
Terraform <romain.cambonie@gmail.com>
2023-04-18 09:14:12 +0200

    feat: adding terraform aws service file

---
+++ ecr.tf
@@ -0,0 +1,10 @@
+resource "aws_ecr_repository" "api" {
+  name                 = "api"
+  image_tag_mutability = "MUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = false
+  }
+
+  tags = local.tags
+}
