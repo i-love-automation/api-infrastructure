0a8b403263397b5d8b62ccdde1d92cb97a9eed98
Terraform <romain.cambonie@gmail.com>
2023-04-19 15:29:24 +0200

    feat: adding terraform aws service file

--- ecs.tf
+++ ecs.tf
@@ -16,54 +16,15 @@ resource "aws_ecs_cluster_capacity_providers" "fargate" {
   }
 }
 
-#data "aws_vpc" "taxi_aymeric_vpc" {
-#  tags = {
-#    Name = "taxi-aymeric-vpc"
-#  }
-#}
-
-# TODO passer sur les subnets privés
-#data "aws_subnet" "public_subnets" {
-#  count  = 2
-#  vpc_id = data.aws_vpc.taxi_aymeric_vpc.id
-#  tags = {
-#    Name = "public_${count.index + 1}"
-#  }
-#}
-#
-#data "aws_lb_target_group" "load_balancer_target_group" {
-#  name = "load-balancer-api-target-group"
-#}
-#
-#data "aws_db_instance" "taxi_rds_instance" {
-#  db_instance_identifier = "taxi"
-#}
-#
-#data "aws_db_instance" "taxi_rds_instance_2" {
-#  db_instance_identifier = "taxi-2"
-#}
-#
-#locals {
-#  connexionString  = "postgres://${var.db_username}:${var.db_password}@${data.aws_db_instance.taxi_rds_instance.endpoint}/${data.aws_db_instance.taxi_rds_instance.db_name}"
-#  connexionString2 = "postgres://${var.db_username}:${var.db_password}@${data.aws_db_instance.taxi_rds_instance_2.endpoint}/${data.aws_db_instance.taxi_rds_instance_2.db_name}"
-#}
-
-
-
-variable "container" {
-  default = ""
-}
-
-variable "container_tag" {
-  default = ""
-}
 resource "aws_ecs_task_definition" "task_definition" {
-  family             = "node-server"
-  execution_role_arn = var.task_definition_execution_role_arn //aws_iam_role.task_definition_execution_role.arn
+  count = var.container_tag ? 1 : 0
+
+  family             = "node-api"
+  execution_role_arn = aws_iam_role.ecs_task_execution_role.arn
   container_definitions = jsonencode([
     {
       name : var.project,
-      image : "${aws_ecr_repository.api.repository_url}:${var.container_tag}", //"860265624594.dkr.ecr.us-east-1.amazonaws.com/${var.container}:${var.container_tag}",
+      image : "${aws_ecr_repository.api.repository_url}:${var.container_tag}",
       environment : [
         { "name" : "PORT", "value" : "80" },
         { "name" : "DATABASE_URL", "value" : var.db_connexion_string }
@@ -102,14 +63,9 @@ resource "aws_ecs_task_definition" "task_definition" {
   requires_compatibilities = ["FARGATE"]
 }
 
-resource "null_resource" "always_run" {
-  triggers = {
-    timestamp = timestamp()
-  }
-}
-
-
 resource "aws_ecs_service" "api_service" {
+  count = var.container_tag ? 1 : 0
+
   name                              = var.project
   cluster                           = aws_ecs_cluster.cluster.id
   task_definition                   = aws_ecs_task_definition.task_definition.arn
@@ -123,16 +79,9 @@ resource "aws_ecs_service" "api_service" {
     container_port   = 80
   }
 
-  # Public IP is the easiest way to be able to pull on ECR: https://stackoverflow.com/questions/61265108/aws-ecs-fargate-resourceinitializationerror-unable-to-pull-secrets-or-registry
   network_configuration {
     security_groups  = [aws_security_group.security_group_api_service.id]
-    subnets          = var.public_subnets_id // data.aws_subnet.public_subnets.*.id
+    subnets          = var.public_subnets_ids
     assign_public_ip = true
-  }
-
-  lifecycle {
-    replace_triggered_by = [
-      null_resource.always_run
-    ]
   }
 }
